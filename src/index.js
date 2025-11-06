89cf42d61dc4be1a46e36d8eb6d2d8337c60d8f8
maprashant <129914669+maprashant@users.noreply.github.com>
2025-11-06 11:47:25 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+// var numbers = [3, 56, 2, 48, 5];function double(number) {
+//   return number * 2;
+// }
+
+// const newNum = numbers.map(double);
+// console.log(newNum);
+
+//Filter - Create a new array by keeping the items that return true.
+// var numbers = [3, 56, 2, 48, 5];
+// const newNumbers = numbers.filter(function (num) {
+//   return num<10;
+// });
+// console.log(newNumbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+// var numbers = [3, 56, 2, 48, 5];
+// const newNumb = numbers.reduce(function (acc, current){
+//     console.log("acc =" + current) //to know what is Accumulator and current
+//     return acc + current ;
+// })
+// console.log(newNumb);
+
+//Find - find the first item that matches from an array.
+// var numbers = [3, 56, 2, 48, 5];
+// const findNum = numbers.find(function (num) {
+//   return num > 10;
+// });
+// console.log(findNum);
+
+//FindIndex - find the index of the first item that matches.
